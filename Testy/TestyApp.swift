55be2d78e6ef7f7b262066d4ed1df5dc98be2149
vexonius <tino@etino.dev>
2024-03-25 14:06:00 +0100

    Initial Commit

---
+++ Testy/TestyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestyApp.swift
+//  Testy
+//
+//  Created by Tino on 25.03.24.
+//
+
+import SwiftUI
+
+@main
+struct TestyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
